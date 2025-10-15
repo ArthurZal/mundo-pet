2926d551ec03d90cdb29f382c4d5862528787b07
Arthur Oliveira <zallarthur@gmail.com>
2025-10-15 06:34:38 -0300

    feat: seleciona campos do formulário para utilizar valores digitados pelo usuário

---
+++ src/modules/modal/form/submit.js
@@ -0,0 +1,24 @@
+import { createNewSchedule } from "../../schedule/create-schedule";
+
+const form = document.querySelector("form");
+const inputTutorName = document.querySelector("#tutor-name");
+const inputPetName = document.querySelector("#pet-name");
+const inputTel = document.querySelector("#tel");
+const inputDescription = document.querySelector("#service-description");
+const inputDate = document.querySelector("#date");
+const inputHours = document.querySelector("#hours-option");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    await createNewSchedule(
+        inputTutorName.value,
+        inputPetName.value,
+        inputTel.value,
+        inputDescription.value,
+        inputDate.value,
+        inputHours.value
+    );
+    
+    console.log("Formulário enviado");
+});
