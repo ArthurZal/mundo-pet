a9a970958588ec647f397a80a9969dc228fcb355
Arthur Oliveira <zallarthur@gmail.com>
2025-10-15 06:34:06 -0300

    feat: faz requisição para criar novo objeto de agendamento

---
+++ src/modules/schedule/create-schedule.js
@@ -0,0 +1,25 @@
+import { apiConfig } from "../../utility/api-config.js";
+
+export async function createNewSchedule(tutorName, petName, tel, serviceDescription, date, time) {
+    try {
+        await fetch(`${apiConfig.baseUrl}/schedules`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                tutorName,
+                petName,
+                tel,
+                serviceDescription,
+                date,
+                time,
+            }),
+        })
+
+        alert("Agendamento criado com sucesso!");
+
+    } catch (error) {
+        alert("Erro ao criar agendamento. Tente novamente.");
+    }
+}
