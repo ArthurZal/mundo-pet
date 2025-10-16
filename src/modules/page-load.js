368c5a512dcd5ebe7814d3fce777facc46990f30
Arthur Oliveira <zallarthur@gmail.com>
2025-10-16 17:17:22 -0300

    feat: define data inicial para input de data e exibe os agendamentos com base na data selecionada

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -1,15 +1,12 @@
+import { dateToday } from "../utility/dateToday";
 import { showSchedules } from "./schedule/show-schedules";
-import dayjs from "dayjs";
 
 document.addEventListener("DOMContentLoaded", () => {
   const dateInput = document.getElementById("date-menu");
-  const dateToday = dayjs().format("YYYY-MM-DD");
-  dateInput.value = dateToday;
-
-  showSchedules(dateToday);
+  dateToday(dateInput);
+  showSchedules();
 
   dateInput.addEventListener("change", (e) => {
-    const selectedDate = dateInput.value;
-    showSchedules(selectedDate);
+    showSchedules();
   });
 });
