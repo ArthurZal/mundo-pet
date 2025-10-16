db03888a0d8fad41c85cf459d0ca40b96b74c08f
Arthur Oliveira <zallarthur@gmail.com>
2025-10-15 23:04:59 -0300

    feat: limpa os capos de inputs do formulário ao fechar o modal

--- src/modules/modal/open-modal.js
+++ src/modules/modal/open-modal.js
@@ -3,11 +3,12 @@ import { defineHours} from "./form/define-hours.js";
 const openModalButton = document.querySelector(".open-modal-button");
 const closeModalButton = document.querySelector(".close-icon");
 const modalContainer = document.querySelector(".modal-container");
+const form = document.querySelector("form");
 
 openModalButton.addEventListener("click", () => {
     modalContainer.classList.remove("disable")
-    defineHours();
-    
+    form.reset();
+    defineHours(); 
 });
 
 closeModalButton.addEventListener("click", () => {
